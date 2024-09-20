5d03919885fa7b08da01d3715d91b76ae460e401
Harri4135 <Oliver_harrison@me.com>
2024-09-19 21:41:11 -0400

    bad data swap

--- lab1-resistance.py
+++ lab1-resistance.py
@@ -12,15 +12,17 @@ verr1 = bb.np.array([0.0052495,0.0053,0.005251,0.005251])
 ierr1 = bb.np.array([0.13076,0.065,0.032878,0.009826])
 
 
-v2 = bb.np.array([6.447,6.265,6.447,6.495])
-i2 = bb.np.array([29.984,2.3263,0.24271,0.06266])
+v2 = bb.np.array([6.387,6.448
+,6.477
+,6.498])
+i2 = bb.np.array([62.91,30.006,13.942,2.414])
 verr2 = bb.np.array([0.00531935,0.0053224,0.00532385,0.0053249])
 ierr2 = bb.np.array([0.062582,0.0560012,0.0077884,0.0054828])
 
 init_guess = (-0.5, 0) # guess for the best fit parameters
 font_size = 12
-xlabel = "Time (s)"
-ylabel = "Height (m)"
+xlabel = "Voltage (V)"
+ylabel = "Current (A)"
 
 # Now we make the plot, displayed on screen and saved in the directory, and print the best fit values
 bb.plot_fit(linear, i1, v1, ierr1, verr1, init_guess=init_guess, font_size=font_size,
