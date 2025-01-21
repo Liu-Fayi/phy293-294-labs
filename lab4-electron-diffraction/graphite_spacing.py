a0a8aef06f3337d54a5bd43f781b4007b897dd69
Liu-Fayi <85021501+Liu-Fayi@users.noreply.github.com>
2025-01-21 17:35:50 -0500

    .

--- lab4-electron-diffraction/graphite_spacing.py
+++ lab4-electron-diffraction/graphite_spacing.py
@@ -1,3 +1,5 @@
+import math
+
 import tools as tl
 import fit_black_box as bb
 import numpy as np
@@ -14,30 +16,33 @@ def linear(t, a, b):
 def power(t, a, b):
     return a*t**b
 
+def proportional(t, m):
+    return m*t
+
 r = np.array([15.825, 15, 14.6, 14.225, 13.3, 13, 12.5, 12.35, 11.95, 11.7, 11.5, 11.125])
 V = np.array([2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.4, 4.6, 4.8])
 r = r / 1000
 V = V * 1000
-rerr = 0.0005
+rerr = 0.2/1000
 Verr = 100
 
 log_V, log_r = tl.log_log(V, r)
-log_Verr = tl.error_log(V, Verr, 10)
-log_rerr = tl.error_log(r, rerr, 10)
+log_Verr = tl.error_log(V, Verr, math.e)
+log_rerr = tl.error_log(r, rerr, math.e)
 
 initial_guess = [1, 1]
-bb.plot_fit(linear, log_V, log_r, log_Verr, log_rerr, init_guess=initial_guess, font_size=20, xlabel="log(V)", ylabel="log(r)")
+bb.plot_fit(linear, log_V, log_r, log_Verr, log_rerr, init_guess=initial_guess, font_size=20, xlabel="log(Voltage)", ylabel="log(radius)")
 # bb.plot_fit(power, V, r, Verr, rerr, init_guess=initial_guess, font_size=20, xlabel="V", ylabel="r")
 
-m_fit, m_fit_err = -0.5717114960163332, 0.015307489242173648
-b_fit, b_fit_err = 0.34510471251841573, 0.12487732562755154
+m_fit, m_fit_err = -0.5717114431915732, 0.015307491587896769
+b_fit, b_fit_err = 0.34510428139650384, 0.1248773416094752
 
 coeff = np.exp(b_fit)
-coeff_err = tl.error_exp2(b_fit, b_fit_err, np.exp(b_fit))
-b = 2*R*n*h/(np.sqrt(2*m*e)*coeff)
-b_err = tl.error_exp(coeff, coeff_err, -0.5, 1/coeff**0.5)
-b_err = tl.error_mult(1/coeff**0.5, 2*R*n*h/(np.sqrt(2*m*e)), b_err, 0, b)
-print(f"b: {b} +/- {b_err}")
+coeff_err = tl.error_exp2(math.e, b_fit, b_fit_err)
+d = 2*R*n*h/(np.sqrt(2*m*e)*coeff)
+d_err = tl.error_exp(coeff, coeff_err, -0.5, 1/coeff**0.5)
+d_err = tl.error_mult(1/coeff**0.5, 2*R*n*h/(np.sqrt(2*m*e)), d_err, 0, b)
+print(f"b: {d} +/- {d_err}")
 
 log_r_predicted = linear(log_V, m_fit, b_fit)
 
---
+++ lab4-electron-diffraction/wavelength_inner_radius_plot.py
@@ -0,0 +1,51 @@
+import math
+
+import tools as tl
+import fit_black_box as bb
+import numpy as np
+
+h = 6.62607015e-34
+R = 0.065
+n = 1
+m = 9.10938356e-31
+e = 1.60217662e-19
+
+def proportional(t, m):
+    return m*t
+
+r = np.array([15.825, 15, 14.6, 14.225, 13.3, 13, 12.5, 12.35, 11.95, 11.7, 11.5, 11.125])
+V = np.array([2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.4, 4.6, 4.8])
+r = r / 1000
+V = V * 1000
+rerr = 0.2/1000
+Verr = 100
+
+
+lam = h / (np.sqrt(2*m*e*V))
+lam_err = tl.error_mult(V, 2*m*e, Verr, 0, V*2*m*e)
+lam_err = tl.error_exp(2*m*e*V, lam_err, -0.5, 1/np.sqrt(2*m*e*V))
+lam_err = tl.error_mult(1/np.sqrt(2*m*e*V), h, lam_err, 0, lam)
+
+
+bb.plot_fit(proportional, lam, r, lam_err, rerr, font_size=20, xlabel="Wavelength (m)", ylabel="Radius (m)")
+
+m_fit = 641291494.7007778
+m_fit_err = 3233288.4802381983
+
+d = 2 * R * n / m_fit
+d_err = tl.error_mult(m_fit, 2, m_fit_err, 0, d)
+print(f"d: {d} +/- {d_err}")
+
+r_predicted = proportional(lam, m_fit)
+
+rmse = tl.rmse(r, r_predicted)
+print(f"RMSE: {rmse}")
+
+chi2 = tl.chi_squared(r, r_predicted, rerr)
+print(f"Chi squared: {chi2}")
+
+red_chi2 = tl.reduced_chi_squared(chi2, 1, 12)
+print(f"Reduced chi squared: {red_chi2}")
+
+r2 = tl.r_sq(r, r_predicted)
+print(f"R^2: {r2}")
